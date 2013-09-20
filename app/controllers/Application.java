00591cba3fc8545365c303fec5f1f5bcf2078249
Benoît Simard <bsimard@logisima.com>
2013-09-20 23:17:14 +0200

    admin

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -14,13 +14,13 @@ public class Application extends AbstractController {
      * Home page.
      */
     public static void index() {
-        List<CMSPage> blogs = CMSPage.getLastests("blog", 3);
-        List<CMSPage> projects = CMSPage.getLastests("project", 3);
-        render();
+        List<CMSPage> blogs = CMSPage.getAllByTemplate("blog");
+        List<CMSPage> projects = CMSPage.getAllByTemplate("project");
+        render(blogs, projects);
     }
 
     /**
-     * Home page.
+     * Blog page.
      */
     public static void blog() {
         CMSPage page = CMSPage.getLastest("blog");
