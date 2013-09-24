c49eeb9ae7a447fdaee5f358bc74d6903fc4e19b
Benoît Simard <bsimard@logisima.com>
2013-09-24 08:51:50 +0200

    search & responsive

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -2,13 +2,20 @@ package controllers;
 
 import controllers.cms.Admin;
 import models.cms.CMSPage;
+import play.modules.search.Query;
+import play.modules.search.Search;
 
+import java.util.HashMap;
 import java.util.List;
 
 /**
  * Controller that managed principal page of the site.
  */
 public class Application extends AbstractController {
+
+    public final static int ITEM_PER_PAGE = 5;
+    public static final String MAP_RESULT_LIST = "result";
+    public static final String MAP_RESULT_NB = "nb";
 
     /**
      * Home page.
@@ -29,8 +36,22 @@ public class Application extends AbstractController {
     /**
      * Search action.
      */
-    public static void search(String search) {
-        render();
+    public static void search(String search, Integer page) {
+        if (page == null) {
+            page = 1;
+        }
+        // default search
+        String query = search;
+        if(search == null || search.trim().length() == 0) {
+            query = "*:*";
+        }
+
+        query += " AND (template:'blog' OR template:'page')";
+        Query q = Search.search(query, CMSPage.class);
+        List<CMSPage> pages = q.page((page-1) * ITEM_PER_PAGE, ITEM_PER_PAGE).fetch();
+        Integer nbItems = q.count();
+
+        render(search, page, pages, nbItems);
     }
 
     /**
